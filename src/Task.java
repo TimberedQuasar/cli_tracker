2695a6e751b284d132f986ce694a72f3cb839091
TimberedQuasar <szymon.j.jancewicz@gmail.com>
2026-01-07 14:46:27 +0100

    updating-class-v0.2

--- src/Task.java
+++ src/Task.java
@@ -1,7 +1,7 @@
 import java.time.LocalDateTime;
 
 public class Task {
-    private int id = 0;
+    private int id;
     private String description;
     private Status status;
     private LocalDateTime createdAt;
@@ -13,9 +13,11 @@ public class Task {
         COMPLETED
     }
 
+    private static int idCounter = 0;
+
     public void createTask(String description) {
         //i need function to will keep track of id number and status
-        this.id = id++;
+        this.id = idCounter++;
         this.description = description;
         this.status = Status.PENDING;
         this.createdAt = LocalDateTime.now();
