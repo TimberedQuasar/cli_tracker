2c30e28fb5c4884ac2368a29b43f5254376adf9a
TimberedQuasar <szymon.j.jancewicz@gmail.com>
2026-01-07 14:31:47 +0100

    creating-object

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,25 @@
+import java.time.LocalDateTime;
+
 public class Task {
+    private int id = 0;
+    private String description;
+    private enum status {
+        PENDING,
+        IN_PROGRESS,
+        COMPLETED
+    }
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private status status;
+
+    public void createTask(String description) {
+        //i need function to will keep track of id number and status
+        this.id = id++;
+        this.description = description;
+        this.status = status.PENDING;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
 }
