eff6fb0d196f3a33b6740d478732d16182e27281
TimberedQuasar <szymon.j.jancewicz@gmail.com>
2026-01-07 14:42:40 +0100

    updating-class

--- src/Task.java
+++ src/Task.java
@@ -3,21 +3,21 @@ import java.time.LocalDateTime;
 public class Task {
     private int id = 0;
     private String description;
-    private enum status {
+    private Status status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private enum Status {
         PENDING,
         IN_PROGRESS,
         COMPLETED
     }
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-
-    private status status;
 
     public void createTask(String description) {
         //i need function to will keep track of id number and status
         this.id = id++;
         this.description = description;
-        this.status = status.PENDING;
+        this.status = Status.PENDING;
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
     }
