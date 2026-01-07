ef9a8db0c9ca5b20e36edef67667107ce8f27f05
TimberedQuasar <szymon.j.jancewicz@gmail.com>
2026-01-07 15:06:53 +0100

    creating-task

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,2 +1,17 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskManager {
+    private List<Task> tasks;
+
+    public TaskManager() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public Task addTask (String description) {
+        Task task = new Task(description);
+        tasks.add(task);
+        return task;
+    }
 }
